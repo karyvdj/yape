b170e2dcda14ed448f33d32a78722406fcc1a91d
karyvdj <kaina51@hotmail.com>
2017-07-18 14:38:03 -0500

    Primera pnatalla terminada aun faltan detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
- $('.carousel.carousel-slider').carousel({fullWidth: true});
+// var cargarPagina = function(){
+//   cargarApi();
+// }
+//
+// var cargarApi = function () {
+//   $.getJSON(api.url, function (api) {
+//     console.log(api)
+//   });
+// };
+
+//carrusel
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+// $(document).ready(cargarPagina);
