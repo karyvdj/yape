8844effe807b9a78c87757ce48afe30abe254a6a
Kary V Dj <KaryVDj@iMac-de-Kary.local>
2017-07-19 19:54:04 -0500

    validacion de funcionado correctamente

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,52 +1,28 @@
 function cargarpagina() {
-	 $('.carousel.carousel-slider').carousel({fullWidth: true});
+	 carrusel();
 	 validarTel();
 }
 
+function carrusel() {
+	$('.carousel.carousel-slider').carousel({
+		fullWidth: true
+	});
+}
 var $inputTel = $("#input-tel");
 var $checkboxTel = $("#checkbox-tel");
 var $btnContinuar = $("#continuar");
 var $formularioTel= $("#formulario-tel");
-
-console.log($checkboxTel);
-
+//console.log($checkboxTel);
 $inputTel.keyup(validarTel);
+$checkboxTel.change(validarTel);
 
 function validarTel() {
 	var $tel = $inputTel.val().length;
-	if ($tel == 10) {
-		$btnContinuar.removeAttr("disabled");
-		$checkboxTel.attr("checked", true);
+	if ($tel == 10 && $checkboxTel.prop("checked")) {
+		$btnContinuar.removeClass("disabled");
 	}else{
-		$btnContinuar.attr("disabled", true);
-		$checkboxTel.removeAttr("checked");
+		$btnContinuar.addClass("disabled");
 	}
 };
-
-// url: http//localhost:3000/api/registerNumber
-// metodo http: POST
-// data: {"phone":"...","terms": "..."}
-
-// Petición al servidor
-function getJSON(url) {
-	return new Promise(function (resolve, reject) {
-		var ajax = new XMLHttpRequest();
-
-		ajax.open("GET", url),
-		ajax.send();
-		ajax.onreadystatechange = function () {
-			if (ajax.readyState == 4) {
-				// resolve(JSON.parse(ajax.responseText));
-			}
-		}
-	});
-}
-
-getJSON("api/users.js")
-.then(function (datos) {
-	console.log(datos);
-	return getJSON(dato);
-});
-
 
 $(document).ready( cargarpagina);
